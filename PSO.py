a806dc2620061a9dc15b806405a3a210694a8c34
redfire4384 <63990668+redfire4384@users.noreply.github.com>
2023-12-05 09:25:07 -0800

    Add files via upload

--- PSO.py
+++ PSO.py
@@ -1,35 +1,67 @@
 import pygame
+import random
+import numpy as np 
 
 pygame.init()
-screen_width, screen_height = 800, 600
+screen_width, screen_height = 400, 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 agent_radius = 5
 
+def make_height_map():
+    map = np.random.random((screen_width, screen_height))
+    return map
+
 class Agent:
+    def __init__(self, pos):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.vel = 5
+        self.fitness = None
+        self.p_best = pos
+        self.p_best_fit = None
+
+    def get_fitness(self):
+        
+        return self.fitness
+
+    def update_position(self, p_best, p_best_fit):
+        self.x += self.vel
+        self.y += self.vel
+        if self.x > screen_width:
+            self.x = 0
+        if self.y > screen_height:
+            self.y = 0
+
+class Swarm:
     def __init__(self):
-        self.x = screen_width // 2
-        self.y = screen_height // 2
+        self.agents = [Agent((random.randrange(0, screen_width), random.randrange(0, screen_height))) for i in range(10)]
+        self.g_best = None
 
-    def update_position(self):
-        self.x += 1
-        self.y += 1
 
+def update_velocity(agents, base_vel):
+    new_velocity = 1
+
+    return new_velocity
 
 def main():
-    agents = [Agent() for i in range(1)]
+    swarm = Swarm()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
-        for agent in agents:
+        for agent in swarm.agents:
             pygame.draw.circle(screen, (255, 255, 255), (agent.x, agent.y), agent_radius)
-            agent.update_position()
+            agent.vel = update_velocity(swarm, agent.vel)
+            agent.update_position(1, 2)
+        
+        #swarm.find
 
         pygame.display.flip()
         clock.tick(60)
         screen.fill((0,0,0))
 
+print(make_height_map())
 main()
